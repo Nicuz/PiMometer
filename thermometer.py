24e209d29e0a0506412cb8d3a53d32c6a20bc441
Nicuz <nico.majorana@gmail.com>
2017-08-18 18:08:10 +0200

    Update thermometer.py

--- thermometer.py
+++ thermometer.py
@@ -17,7 +17,7 @@ lcd_backlight = 4
 lcd_columns   = 16
 lcd_rows      = 2
 
-#Display inizialization
+#Display initialization
 lcd = Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
 
 #DHT11
